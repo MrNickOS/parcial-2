41e95521c9b49e93341830af611f2bfb56940b09
MrNickOS <nicomac11@hotmail.com>
2016-11-08 18:37:51 -0500

    Delete files_recientes.py

--- files_recientes.py
+++
@@ -1,6 +0,0 @@
-from subprocess import Popen, PIPE
-
-def get_all_recent():
-   elProceso = Popen(["find","-type","f","-mmin","-60"], stdout=PIPE)
-   rec_list = Popen(["awk",'-F','/','{print $NF}'],stdin=elProceso.stdout, stdout=PIPE).communicate()[0].split('\n')
-   return filter(None,rec_list)
